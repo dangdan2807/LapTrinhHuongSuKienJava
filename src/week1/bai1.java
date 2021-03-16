0199db7b2aff91449efcf98d29fc5d8ee8ea0951
KayJuno <dangdan2807@gmail.com>
2021-03-17 00:31:10 +0700

    update: thêm bắt lỗi bai1

--- src/week1/bai1.java
+++ src/week1/bai1.java
@@ -130,12 +130,15 @@ public class bai1 extends JFrame implements ActionListener {
         // btn
         btnGiai = new JButton("Giải");
         btnGiai.setFont(new Font("Arial", Font.BOLD, 14));
+        btnGiai.setMnemonic(KeyEvent.VK_G);
 
         btnDelete = new JButton("Xoá rỗng");
         btnDelete.setFont(new Font("Arial", Font.BOLD, 14));
-        
+        btnDelete.setMnemonic(KeyEvent.VK_X);
+
         btnExit = new JButton("Thoát");
         btnExit.setFont(new Font("Arial", Font.BOLD, 14));
+        btnExit.setMnemonic(KeyEvent.VK_T);
         // thêm nút vào south panel
         pnSouth.add(btnGiai);
         pnSouth.add(btnDelete);
@@ -156,9 +159,22 @@ public class bai1 extends JFrame implements ActionListener {
     public void actionPerformed(ActionEvent e) {
         Object obj = e.getSource();
         double a = 0, b = 0, c = 0, delta = 0, x1, x2;
+
         if (obj.equals(btnExit))
             System.exit(0);
-        else if (obj.equals(btnGiai)) {
+        else if (obj.equals(btnDelete)) {
+            tfInput1.setText("");
+            tfInput2.setText("");
+            tfInput3.setText("");
+            tfInput4.setText("");
+        } else if (obj.equals(btnGiai)) {
+            if (!isNumber(tfInput1))
+                focusTextField(tfInput1);
+            else if (!isNumber(tfInput2))
+                focusTextField(tfInput2);
+            else if (!isNumber(tfInput3))
+                focusTextField(tfInput3);
+            else {
                 a = Double.parseDouble(tfInput1.getText());
                 b = Double.parseDouble(tfInput2.getText());
                 c = Double.parseDouble(tfInput3.getText());
@@ -166,30 +182,35 @@ public class bai1 extends JFrame implements ActionListener {
                     // a== 0 phuong trinh tro thanh phuong trinh bac mot bx + c = 0
                     if (b == 0) {
                         if (c == 0)
-                            tfInput4.setText("Phương trình vô số nghiệm");
+                            tfInput4.setText("Vô số nghiệm");
                         else
-                            tfInput4.setText("Phương trình vô nghiệm");
+                            tfInput4.setText("Vô nghiệm");
                     } else {
-                        tfInput4.setText("Phương trình có nghiệm duy nhất: x = " + -1 * c / b);
+                        tfInput4.setText("Nghiệm duy nhất: x = " + -1 * c / b);
                     }
                 } else {
                     delta = b * b - 4 * a * c;
                     if (delta > 0) {
                         x1 = (-b + Math.sqrt(delta)) / (2 * a);
                         x2 = (-b - Math.sqrt(delta)) / (2 * a);
-                        tfInput4.setText("Phương trình 2 có nghiệm: x1 = " + x1 + ", x2 = " + x2);
+                        tfInput4.setText("Có 2 nghiệm: x1 = " + x1 + ", x2 = " + x2);
                     } else if (delta == 0) {
-                        tfInput4.setText("Phương trình 2 có nghiệm: x1 = x2 = " + -1 * b / 2 * a);
+                        tfInput4.setText("Nghiệm kép: x1 = x2 = " + (-b / 2 * a));
                     } else {
-                        tfInput4.setText("Phương trình vô nghiệm");
+                        tfInput4.setText("Vô nghiệm");
                     }
                 }
-        } else if (obj.equals(btnDelete)) {
-            tfInput1.setText("");
-            tfInput2.setText("");
-            tfInput3.setText("");
-            tfInput4.setText("");
+            }
         }
     }
 
+    public boolean isNumber(JTextField text) {
+        return text.getText().matches("^-?\\d*\\.?\\d+$");
+    }
+
+    public void focusTextField(JTextField text) {
+        JOptionPane.showMessageDialog(null, "Lỗi nhập liệu.");
+        text.selectAll();
+        text.requestFocus();
+    }
 }
